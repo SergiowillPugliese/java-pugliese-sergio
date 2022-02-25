c9b2e03cb213c6cfc63972d4364aa981183a8c8f
Sergio Pugliese <pugliese.sergio1987@hotmail.it>
2022-02-25 09:53:21 +0100

    lezione5

---
+++ Lezione4/prova-intellij/src/com/engim/Bottiglia.java
@@ -0,0 +1,54 @@
+package com.engim;
+/*
+* fare due metodi: riempi(int q) e svuota(int q) che riempiono e svuotano la bottiglia della quantità q
+* BottigliaConTappo
+* boolean aperta
+* apri() e chiudi()
+*  */
+
+
+public class Bottiglia {
+    private int capacita;
+    private int quantita;
+
+    public Bottiglia(int capacita) {
+        if(capacita < 0)
+            this.capacita = 0;
+        else
+            this.capacita = capacita;
+        this.quantita = 0;
+
+    }
+
+
+    public int getCapacita() {
+        return capacita;
+    }
+
+
+    public int getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
+
+    @Override
+    public String toString() {
+        return "B[" +
+                "capacita:" + capacita +
+                ", quantita:" + quantita +
+                ']';
+    }
+
+    public static void riempi(int q){
+        if (q = 0){
+
+        }
+    }
+
+    public static void svuota(int q){
+
+    }
+}
